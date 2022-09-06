c16d8630c86a67285b9fc4d8e3e78c902b937fee
Nenad Skrbic <nenad.skrbic1917@gmail.com>
2022-09-06 12:34:01 +0200

    first commit

---
+++ src/actions.js
@@ -0,0 +1,23 @@
+import { bindActionCreators } from "redux";
+import * as actions from "./actionTypes";
+
+export const bugAdded = (description) => ({
+  type: actions.BUG_ADDED,
+  payload: {
+    description,
+  },
+});
+
+export const bugRemoved = (id) => ({
+  type: actions.BUG_REMOVED,
+  payload: {
+    id: 1,
+  },
+});
+
+export const bugResolved = (id) => ({
+  type: actions.BUG_RESOLVED,
+  payload: {
+    id,
+  },
+});
---
+++ src/reducer.js
@@ -0,0 +1,42 @@
+import { BUG_ADDED, BUG_REMOVED, BUG_RESOLVED } from "./actionTypes";
+//import * as actions from "./actionTypes";
+
+let lastId = 0;
+
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case BUG_ADDED:
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case BUG_REMOVED:
+      return state.filter((bug) => bug.id !== action.payload.id);
+    case BUG_RESOLVED:
+      return state.map((bug) =>
+        bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+      );
+    default:
+      return state;
+  }
+
+  //   SAME BUT WITH IF & ELSE IF
+
+  //   if (action.type === "bugAdded")
+  //     return [
+  //       ...state,
+  //       {
+  //         id: ++lastId,
+  //         description: action.payload.description,
+  //         resolved: false,
+  //       },
+  //     ];
+  //   else if (action.type === "bugRemoved")
+  //     return state.filter((bug) => bug.id !== action.payload.id);
+
+  //   return state;
+}
